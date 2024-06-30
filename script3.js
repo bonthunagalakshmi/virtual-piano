b935784cab33b89319765388514fcde3f0bf4f7e
bonthunagalakshmi <155577446+bonthunagalakshmi@users.noreply.github.com>
2024-06-30 21:30:20 +0530

    Add files via upload

--- script3.js
+++ script3.js
@@ -1,8 +1,15 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const keysCheckbox = document.querySelector(".keys-checkbox input");
+const recordButton = document.getElementById('recordButton');
+const stopButton = document.getElementById('stopButton');
+const playButton = document.getElementById('playButton');
+
 let allKeys = [];
 let audio = new Audio("tunes/a.wav");
 
+let isRecording = false;
+let recordedNotes = [];
+let startTime = null;
 const playTune = (key) => {
     audio.src = `tunes/${key}.wav`;
     audio.play().then(() => {
@@ -16,6 +23,10 @@ const playTune = (key) => {
         setTimeout(() => {
             clickedKey.classList.remove("active");
         }, 150);
+    }
+    if (isRecording) {
+        const time = Date.now() - startTime;
+        recordedNotes.push({ note: key, time });
     }
 };
 
@@ -34,3 +45,31 @@ const pressedKey = (e) => {
 
 keysCheckbox.addEventListener("click", showHideKeys);
 document.addEventListener("keydown", pressedKey);
+
+recordButton.addEventListener('click', () => {
+    isRecording = true;
+    recordedNotes = [];
+    startTime = Date.now();
+    recordButton.disabled = true;
+    stopButton.disabled = false;
+    playButton.disabled = true;
+});
+
+stopButton.addEventListener('click', () => {
+    isRecording = false;
+    recordButton.disabled = false;
+    stopButton.disabled = true;
+    playButton.disabled = false;
+});
+
+playButton.addEventListener('click', () => {
+    if (recordedNotes.length === 0) return;
+
+    let prevTime = 0;
+    recordedNotes.forEach(note => {
+        setTimeout(() => {
+            playTune(note.note);
+        }, note.time - prevTime);
+        prevTime = note.time;
+    });
+});
